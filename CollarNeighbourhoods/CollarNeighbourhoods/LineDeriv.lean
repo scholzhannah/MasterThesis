a776e6f9ad82dd41349570c20a160ba4aa6e5ddf
Hannah Scholz <hannah@scholzbande.de>
2026-07-31 17:38:19 +0200

    some progress for dderivatives along

--- CollarNeighbourhoods/CollarNeighbourhoods/LineDeriv.lean
+++ CollarNeighbourhoods/CollarNeighbourhoods/LineDeriv.lean
@@ -80,49 +80,79 @@ lemma helper1 {p : M} (v : TangentSpace I p) :
 
 #check MDifferentiableOn.mdifferentiableAt
 
--- lemma about when the inverse of the extChartAt is differentiable
-lemma mdifferentiableWithinAt_extChartAt {𝕜 : Type u_1} [NontriviallyNormedField 𝕜] {E : Type u_2}
-    [NormedAddCommGroup E] [NormedSpace 𝕜 E] {H : Type u_3} [TopologicalSpace H]
-    {I : ModelWithCorners 𝕜 E H}
-    {M : Type u_4} [TopologicalSpace M] [ChartedSpace H M] [IsManifold I 1 M] {x : M} :
-    MDiffAt[(extChartAt I x).target] (extChartAt I x).symm ((extChartAt I x) x) := by
-  simp
-
-  sorry
-
 -- use this instead probabaly
 #check mdifferentiableWithinAt_extChartAt_symm
 
-lemma hahaaja {M : Type*}
+lemma jsdoa {M : Type*}
     [TopologicalSpace M] {n : ℕ} [NeZero n] [ChartedSpace (EuclideanHalfSpace n) M]
     [Fact (finrank ℝ E = n)] [IsManifold (𝓡∂ n) ∞ M] {p : M} (hp : (𝓡∂ n).IsBoundaryPoint p)
-    (v : TangentSpace (𝓡∂ n) p) (hv : 0 < (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p v).ofLp 0)
-    (f : M → ℝ)
-    (hf : MDifferentiableAt (𝓡∂ n) 𝓘(ℝ, ℝ) f p) :
+    (v : TangentSpace (𝓡∂ n) p) (hv : 0 < (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p v).ofLp 0) :
+    MDiffAt[Ici 0] (fun (i : ℝ) ↦
+      ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p +
+        i • (mvfderiv (𝓡∂ n) ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p) v) 0 := by
+  apply MDifferentiableWithinAt.add
+  · exact mdifferentiableWithinAt_const
+  · apply mdifferentiableWithinAt_id.smul
+    exact mdifferentiableWithinAt_const
+
+
+lemma thinkofnamelater {M : Type*}
+    [TopologicalSpace M] {n : ℕ} [NeZero n] [ChartedSpace (EuclideanHalfSpace n) M]
+    [Fact (finrank ℝ E = n)] [IsManifold (𝓡∂ n) ∞ M] {p : M} (hp : (𝓡∂ n).IsBoundaryPoint p)
+    (v : TangentSpace (𝓡∂ n) p) (hv : 0 < (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p v).ofLp 0) :
     MDifferentiableWithinAt 𝓘(ℝ, ℝ) (𝓡∂ n) ((extChartAt (𝓡∂ n) p).symm ∘
       fun (i : ℝ) ↦ (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v)
         (Ici (0 : ℝ)) (0 : ℝ) := by
+  have h1 : MDifferentiableWithinAt 𝓘(ℝ, EuclideanSpace ℝ (Fin n)) (𝓡∂ n)
+      (extChartAt (𝓡∂ n) p).symm (extChartAt (𝓡∂ n) p).target
+      ((extChartAt (𝓡∂ n) p) p + (0 : ℝ) • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) := by
+    apply MDifferentiableWithinAt.mono (extChartAt_target_subset_range p)
+    apply mdifferentiableWithinAt_extChartAt_symm
+    simp [mem_extChartAt_target p, -extChartAt]
+  have h2 : MDiffAt[Ici 0] (fun (i : ℝ) ↦
+      (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) 0 :=
+    mdifferentiableWithinAt_const.add (mdifferentiableWithinAt_id.smul
+      mdifferentiableWithinAt_const)
+  let V := (fun (i : ℝ) ↦
+      (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) ⁻¹'
+      (extChartAt (𝓡∂ n) p).target
+  apply MDifferentiableWithinAt.congr_nhds (s := V ∩ Ici 0)
+  · apply h1.comp 0 (h2.mono inter_subset_right)
+    exact inter_subset_left
+  · refine nhdsWithin_inter_of_mem ?_
+    apply ContinuousWithinAt.preimage_mem_nhdsWithin'
+    · fun_prop
+    · rw [zero_smul, add_zero]
+      apply nhdsWithin_mono (t := {x | 0 ≤ x.ofLp 0})
+      · rw [image_subset_iff]
+        intro x hx
+        rw [mem_preimage]
+        simp only [mem_ofPred_eq, PiLp.add_apply, PiLp.smul_apply, smul_eq_mul]
+        apply add_nonneg
+        · apply le_of_eq
+          simpa [ModelWithCorners.isBoundaryPoint_iff, range_modelWithCornersEuclideanHalfSpace,
+            frontier_halfSpace, modelWithCornersEuclideanHalfSpace_apply,
+            -modelWithCornersEuclideanHalfSpace_toFun] using hp
+        · change 0 ≤ x • ((mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v).ofLp 0
+          exact smul_nonneg hx hv.le
+      · rw [extChartAt_coe p]
+        rw [extChartAt_target (𝓡∂ n) p]
+        simp only [comp_apply, inter_mem_iff]
+        refine ⟨?_, ?_⟩
+        · apply (𝓡∂ n).continuousWithinAt_symm.preimage_mem_nhdsWithin
+          rw [ModelWithCorners.left_inv]
+          exact chart_target_mem_nhds (EuclideanHalfSpace n) p
+        · rw [range_modelWithCornersEuclideanHalfSpace n]
+          exact self_mem_nhdsWithin
 
-  sorry
+theorem ContinuousLinearMap.zero_add (R₁ : Type u_1) [Semiring R₁] (M₁ : Type u_4)
+    [TopologicalSpace M₁] [AddCommMonoid M₁] [Module R₁ M₁] [ContinuousAdd M₁]
+    (M₂ : Type u_4) [TopologicalSpace M₂] [AddCommMonoid M₂] [Module R₁ M₂] [ContinuousAdd M₂]
+    (f : M₁ →L[R₁] M₂) :
+    (0 : M₁ →L[R₁] M₂) + f = f := by
+  simp
 
-instance {n : ℕ} [NeZero n] : Add (EuclideanHalfSpace n) where
-  add x y := ⟨x.1 + y.1, add_nonneg x.2 y.2⟩
-
-instance {n : ℕ} [NeZero n] : SMul NNReal (EuclideanHalfSpace n) where
-  smul r x := ⟨r • x.1, smul_nonneg r.2 x.2⟩
-
-lemma modelWithCornersEuclideanHalfSpace_add {n : ℕ} [NeZero n] (x y : EuclideanSpace ℝ (Fin n))
-    (hx : 0 ≤ x.ofLp 0)
-    (hy : 0 ≤ y.ofLp 0) :
-    (𝓡∂ n).symm (x + y) = (𝓡∂ n).symm x + (𝓡∂ n).symm y := by
-  change (𝓡∂ n).symm (⟨x + y, add_nonneg hx hy⟩ : EuclideanHalfSpace n) =
-    (𝓡∂ n).symm (⟨x, hx⟩ : EuclideanHalfSpace n) + (𝓡∂ n).symm
-    (⟨y, hy⟩ : EuclideanHalfSpace n)
-  rw [modelWithCornersEuclideanHalfSpace_symm_val_apply,
-    modelWithCornersEuclideanHalfSpace_symm_val_apply,
-    modelWithCornersEuclideanHalfSpace_symm_val_apply]
-  rfl
-
+set_option backward.isDefEq.respectTransparency false in
 lemma MDerivAlong_eq {M : Type*}
     [TopologicalSpace M] {n : ℕ} [NeZero n] [ChartedSpace (EuclideanHalfSpace n) M]
     [Fact (finrank ℝ E = n)] [IsManifold (𝓡∂ n) ∞ M] {p : M} (hp : (𝓡∂ n).IsBoundaryPoint p)
@@ -133,70 +163,32 @@ lemma MDerivAlong_eq {M : Type*}
   unfold MDerivAlongWithin
   letI y := (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v
   rw [mvfderiv_comp_mvfderivWithin 0 ((helper1 v).symm ▸ hf)]
-  · sorry
-  · unfold extChartAt
-    rw [OpenPartialHomeomorph.extend_coe_symm, comp_assoc]
-
-    -- mdifferentiableAt_symm_of_mem_maximalAtlas
-    let g := ↑(𝓡∂ n).symm ∘ fun (i : ℝ) ↦
-        ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p +
-          i • (mvfderiv (𝓡∂ n) ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p) v
-    have h1 : MDifferentiableAt (𝓡∂ n) (𝓡∂ n) (chartAt (EuclideanHalfSpace n) p).symm (g 0) := by
-      apply mdifferentiableAt_symm_of_mem_maximalAtlas (IsManifold.chart_mem_maximalAtlas p)
-
+  · simp only [comp_apply, ContinuousLinearMap.comp_apply]
+    rw [zero_smul, add_zero]
+    rw [PartialEquiv.left_inv (extChartAt (𝓡∂ n) p) (mem_extChartAt_source p)]
+    refine Eq.symm (DFunLike.congr rfl ?_)
+    have h1 : mfderiv[Ici (0 : ℝ)] (fun (i : ℝ) ↦
+        (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) 0 1
+        = (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v := by
+      rw [mfderivWithin_eq_fderivWithin]
+      simp only [fderivWithin_const_add]
+      rw [fderivWithin_smul_const (uniqueDiffWithinAt_Ici 0) differentiableWithinAt_fun_id,
+        fderivWithin_fun_id (uniqueDiffWithinAt_Ici 0)]
+      rw [zero_smul, add_zero]
+      -- avoiding some mysterious defeq issues
+      change ((ContinuousLinearMap.id ℝ ℝ).smulRight
+        ((mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v)) 1 = _
+      rw [ContinuousLinearMap.smulRight_apply]
+      simp
+    have : MDiffAt[Ici (0 : ℝ)] (fun (i : ℝ) ↦
+        (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) 0 := by
       sorry
-    --have := mdifferentiableAt_symm_of_mem_maximalAtlas
-    apply h1.comp_mdifferentiableWithinAt
-    unfold g
-    rw [comp_def]
+    -- I again have major defeq issues here, investigate this later
+    change v = (mfderivWithin _ (𝓡∂ n) ((extChartAt (𝓡∂ n) p).symm ∘
+      fun (i : ℝ) ↦ (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) (Ici 0) 0) 1
+    --rw [mfderivWithin_comp 1]
 
-    have h : 0 ≤ (((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p).ofLp 0 := by
-      sorry
-    have h2 (x : ℝ) : 0 ≤ (x • (mvfderiv (𝓡∂ n)
-      ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p) v).ofLp 0 := by
-      sorry
-    --rw [fun x ↦ modelWithCornersEuclideanHalfSpace_add
-    --  (((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p)
-    --  (x • (mvfderiv (𝓡∂ n) ((chartAt (EuclideanHalfSpace n) p).extend (𝓡∂ n)) p) v)]
     sorry
-    /-
-    have h1 : MDifferentiableWithinAt 𝓘(ℝ, EuclideanSpace ℝ (Fin n)) (𝓡∂ n)
-        (extChartAt (𝓡∂ n) p).symm (extChartAt (𝓡∂ n) p).target
-        ((extChartAt (𝓡∂ n) p) p + (0 : ℝ) • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) := by
-      apply MDifferentiableWithinAt.mono (extChartAt_target_subset_range p)
-      apply mdifferentiableWithinAt_extChartAt_symm
-      simp [mem_extChartAt_target p, -extChartAt]
-    have h2 : MDiffAt[Ici 0] (fun (i : ℝ) ↦
-        (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) 0 :=
-      mdifferentiableWithinAt_const.add (mdifferentiableWithinAt_id.smul
-        mdifferentiableWithinAt_const)
-    let V := (fun (i : ℝ) ↦
-        (extChartAt (𝓡∂ n) p) p + i • (mvfderiv (𝓡∂ n) (extChartAt (𝓡∂ n) p) p) v) ⁻¹'
-        (extChartAt (𝓡∂ n) p).target
-    --have hV : V ⊆ Ici 0 := by
-    -- intro x
-    --  simp [V]
-    --  sorry
-    apply MDifferentiableWithinAt.congr_nhds (s := V ∩ Ici 0)
-
-    · apply h1.comp 0 (h2.mono inter_subset_right)
-      exact inter_subset_left
-    · unfold V
-
-      sorry
-      /-refine nhdsWithin_inter_of_mem ?_
-      apply ContinuousWithinAt.preimage_mem_nhdsWithin' (by fun_prop)
-      simp only [zero_smul, add_zero]
-      apply nhdsWithin_mono (t := {x | 0 ≤ x.ofLp 0})
-      · rw [← mapsTo_iff_image_subset]
-        have : ((extChartAt (𝓡∂ n) p) p).ofLp 0 = 0 := by
-          rw [ModelWithCorners.isBoundaryPoint_iff] at hp
-          rw [range_modelWithCornersEuclideanHalfSpace n] at hp
-          simp_all
-        intro x hx
-        simp only [mem_ofPred_eq, PiLp.add_apply, this, PiLp.smul_apply, smul_eq_mul, zero_add]
-        exact (mul_nonneg_iff_of_pos_right hv).mpr hx
-      ·
-        sorry-/-/
+  · exact thinkofnamelater (E := E) hp v hv
   · rw [uniqueMDiffWithinAt_iff_uniqueDiffWithinAt]
     exact uniqueDiffWithinAt_Ici 0
